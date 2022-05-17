3f6366bba8ff9db3ffb60d72b18d5df92fccce9b
Neil Araujo <41514197+NeilAraujo@users.noreply.github.com>
2022-05-17 11:23:42 -0400

    Delete flownet2 directory

--- flownet2/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python main.py --inference --model FlowNet2 --save_flow --inference_dataset Google \
-	--inference_dataset_root ./../video \
-	--resume ./FlowNet2_checkpoint.pth.tar \
-	--inference_visualize
